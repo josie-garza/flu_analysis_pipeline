efa2fe7b886d888cc3ddae43965c0bd11166f637
Josie Garza <jpg7@rice.edu>
2019-04-01 15:01:42 -0500

    added

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -12,18 +12,8 @@ for i in "${directories[@]}"; do
     mkdir $i
 done
 
-samples=(00001)
+samples=(00001 00002 00003 00004)
 ## Step 03: Recruit reads to a Flu reference
 for j in "${samples[@]}"; do
-    bowtie2 -x ${REFERENCE} \
-    --quiet\
-    -1  /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq\
-    -2  /research/emit/emit/00-reads/split_reads/UMDA_$j.2.fastq\
-    -S 03-bowtie2/$j.sam\
-    --no-unal
-    samtools view -Sb 03-bowtie2/$j.sam > 03-bowtie2/$j.bam
-    samtools sort 03-bowtie2/$j.bam -o 03-bowtie2/$j.sorted.bam
-    samtools index 03-bowtie2/$j.sorted.bam
-    rm 03-bowtie2/$j.sam
-    rm 03-bowtie2/$j.bam
+    echo $j
 done
