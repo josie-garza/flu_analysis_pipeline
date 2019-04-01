d1529d9d777e66d068b113ab84d954f1a87bcc93
Josie Garza <jpg7@rice.edu>
2019-04-01 14:16:57 -0500

    added

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -e
 
-WORKING=$1
-SAMPLE=$2
 REFERENCE="references/flu_ref"
-SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ## Create the working directories
 directories=(03-bowtie2)
 for i in "${directories[@]}"; do
     mkdir $i
-    mkdir -p $i/$SAMPLE
 done
 
+samples=(00001)
 ## Step 03: Recruit reads to a Flu reference
-bowtie2 -x ${REFERENCE} \
-    -1 $WORKING/02-flash/$SAMPLE/out.notCombined_1.fastq \
-    -2 $WORKING/02-flash/$SAMPLE/out.notCombined_2.fastq \
-    -S $WORKING/$SAMPLE/out.sam
+for j in "${directories[@]}"; do
+    echo $j
+done
