2b3ca76ad26f16e46d2e4b122e24cf8673349a9b
Josie Garza <jpg7@rice.edu>
2019-04-01 15:20:22 -0500

    added

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -12,22 +12,27 @@ for i in "${directories[@]}"; do
         mkdir $i
 done
 
-samples=(00001
-00002)
-## Step 03: Recruit reads to a Flu reference
-for j in "${samples[@]}"; do
-    if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
-            echo $j
-            bowtie2 -x ${REFERENCE} \
-            --quiet\
-            -1  /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq\
-            -2  /research/emit/emit/00-reads/split_reads/UMDA_$j.2.fastq\
-            -S 03-bowtie2/$j.sam\
-            --no-unal
-            samtools view -Sb 03-bowtie2/$j.sam > 03-bowtie2/$j.bam
-            samtools sort 03-bowtie2/$j.bam -o 03-bowtie2/$j.sorted.bam
-            samtools index 03-bowtie2/$j.sorted.bam
-            rm 03-bowtie2/$j.sam
-            rm 03-bowtie2/$j.bam
-    fi
+test=()
+for k in {1..5}; do
+        test+=k
 done
+echo ${arr[@]}
+
+# samples=(00001)
+# ## Step 03: Recruit reads to a Flu reference
+# for j in "${samples[@]}"; do
+#     if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
+#             echo $j
+#             bowtie2 -x ${REFERENCE} \
+#             --quiet\
+#             -1  /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq\
+#             -2  /research/emit/emit/00-reads/split_reads/UMDA_$j.2.fastq\
+#             -S 03-bowtie2/$j.sam\
+#             --no-unal
+#             samtools view -Sb 03-bowtie2/$j.sam > 03-bowtie2/$j.bam
+#             samtools sort 03-bowtie2/$j.bam -o 03-bowtie2/$j.sorted.bam
+#             samtools index 03-bowtie2/$j.sorted.bam
+#             rm 03-bowtie2/$j.sam
+#             rm 03-bowtie2/$j.bam
+#     fi
+# done
