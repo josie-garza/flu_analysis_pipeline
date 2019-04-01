5e135615c66fa46da5824850c2079d3a35eedfcc
Josie Garza <jpg7@rice.edu>
2019-04-01 13:28:10 -0500

    basic assem

---
+++ UMDA/script_dev/assembly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+WORKING=$1
+SAMPLE=$2
+REFERENCE="$WORKING/references/flu_ref"
+SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+## Create the working directories
+directories=( 03-bowtie2)
+for i in "${directories[@]}"; do
+    mkdir -p $WORKING/$i
+    if [ -d $WORKING/$i/$SAMPLE ]; then
+	rm -rf $WORKING/$i/$SAMPLE
+    fi
+    mkdir -p $WORKING/$i/$SAMPLE
+done
