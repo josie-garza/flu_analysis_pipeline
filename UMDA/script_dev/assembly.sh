84550535d3438213262a8c2b674b51a349fb3a15
Josie Garza <jpg7@rice.edu>
2019-04-01 14:36:51 -0500

    added

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -13,7 +13,7 @@ samples=(00001)
 ## Step 03: Recruit reads to a Flu reference
 for j in "${samples[@]}"; do
     bowtie2 -x ${REFERENCE} \
-    -q\
+    --quiet\
     -1  00-reads/UMDA_$j.1.fastq\
     -2  00-reads/UMDA_$j.2.fastq\
     -S 03-bowtie2/$j.sam\
