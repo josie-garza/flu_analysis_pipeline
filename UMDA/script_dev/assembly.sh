ab39fc8b62a486c1bf6e79ee0f917aebd97b38be
Josie Garza <jpg7@rice.edu>
2019-04-12 16:29:08 -0500

    updated

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -5,18 +5,18 @@ set -e
 REFERENCE="references/fluA_ny_h3n2.fna"
 
 # Create the directory for the sorted bam files
-directories=(07-genes)
-for i in "${directories[@]}"; do
-        # remove if it already exists
-        if [ -d $i ]; then
-                rm -rf $i
-        fi
-        mkdir $i
-done
+# directories=(04-vcf 05-html 06-annotated_vcf 07-genes)
+# for i in "${directories[@]}"; do
+#         # remove if it already exists
+#         if [ -d $i ]; then
+#                 rm -rf $i
+#         fi
+#         mkdir $i
+# done
 
 # Create a list for the number of samples to run the assembly on
 samples=()
-for k in {1..107}; do  #237 is the number of samples
+for k in {108..237}; do  #237 is the number of samples
         c=$k
         while [ ${#c} -le 4 ]
         do
@@ -49,13 +49,13 @@ done
 # done
 
 # Create a vcf for every sorted bam file
-# for j in "${samples[@]}"; do
-#     # check to make sure the sample exists in the folder
-#     if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
-#             echo $j
-#             lofreq call -f ${REFERENCE} -o 04-vcf/$j.vcf 03-bowtie2/$j.sorted.bam
-#     fi
-# done
+for j in "${samples[@]}"; do
+    # check to make sure the sample exists in the folder
+    if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
+            echo $j
+            lofreq call -f ${REFERENCE} -o 04-vcf/$j.vcf 03-bowtie2/$j.sorted.bam
+    fi
+done
 
 # Update the vcf
 for j in "${samples[@]}"; do
