c55134203771a0ed84b3e9497b9d3dce6e4ec466
Josie Garza <jpg7@rice.edu>
2019-04-12 12:59:54 -0500

    updated

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -5,7 +5,7 @@ set -e
 REFERENCE="references/fluA_ny_h3n2.fna"
 
 # Create the directory for the sorted bam files
-directories=(05-html 06-annotated_vcf)
+directories=(04-vcf 05-html 06-annotated_vcf)
 for i in "${directories[@]}"; do
         # remove if it already exists
         if [ -d $i ]; then
@@ -49,13 +49,13 @@ done
 # done
 
 # Create a vcf for every sorted bam file
-# for j in "${samples[@]}"; do
-#     # check to make sure the sample exists in the folder
-#     if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
-#             echo $j
-#             lofreq call -f ${REFERENCE} -o 04-vcf/$j.vcf 03-bowtie2/$j.sorted.bam
-#     fi
-# done
+for j in "${samples[@]}"; do
+    # check to make sure the sample exists in the folder
+    if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
+            echo $j
+            lofreq call -f ${REFERENCE} -o 04-vcf/$j.vcf 03-bowtie2/$j.sorted.bam
+    fi
+done
 
 # Update the vcf
 for j in "${samples[@]}"; do
