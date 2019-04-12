d00b67c3ae9dc926044368fb9a26c8161fe1b051
Josie Garza <jpg7@rice.edu>
2019-04-12 00:18:01 -0500

    creating vcfS

--- UMDA/script_dev/assembly.sh
+++ UMDA/script_dev/assembly.sh
@@ -4,8 +4,8 @@ set -e
 ## built by bowtie2-build
 REFERENCE="references/flu_ref"
 
-## Create the directoru for the sorted bam files
-directories=(03-bowtie2)
+## Create the directory for the sorted bam files
+directories=(04-vcf)
 for i in "${directories[@]}"; do
         # remove if it already exists
         if [ -d $i ]; then
@@ -25,25 +25,39 @@ for k in {1..237}; do  #237 is the number of samples
         samples+=($c )
 done
 
-## Step 03: Recruit reads to a Flu reference
+## created sorted bam files
+# for j in "${samples[@]}"; do
+#     # check to make sure the sample exists in the folder
+#     if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
+#             echo $j
+#             bowtie2 -x ${REFERENCE} \
+#             --quiet\
+#             -1  /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq\
+#             -2  /research/emit/emit/00-reads/split_reads/UMDA_$j.2.fastq\
+#             -S 03-bowtie2/$j.sam\
+#             --no-unal
+#             # sam to bam
+#             samtools view -Sb 03-bowtie2/$j.sam > 03-bowtie2/$j.bam
+#             # sort the bam
+#             samtools sort 03-bowtie2/$j.bam -o 03-bowtie2/$j.sorted.bam
+#             # create the .bai file for IGV viewing
+#             samtools index 03-bowtie2/$j.sorted.bam
+#             # remove intermediate steps
+#             rm 03-bowtie2/$j.sam
+#             rm 03-bowtie2/$j.bam
+#     fi
+# done
+
+# create a vcf for every sorted bam file
 for j in "${samples[@]}"; do
     # check to make sure the sample exists in the folder
     if [ -f /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq ]; then
             echo $j
-            bowtie2 -x ${REFERENCE} \
-            --quiet\
-            -1  /research/emit/emit/00-reads/split_reads/UMDA_$j.1.fastq\
-            -2  /research/emit/emit/00-reads/split_reads/UMDA_$j.2.fastq\
-            -S 03-bowtie2/$j.sam\
-            --no-unal
-            # sam to bam
-            samtools view -Sb 03-bowtie2/$j.sam > 03-bowtie2/$j.bam
-            # sort the bam
-            samtools sort 03-bowtie2/$j.bam -o 03-bowtie2/$j.sorted.bam
-            # create the .bai file for IGV viewing
-            samtools index 03-bowtie2/$j.sorted.bam
-            # remove intermediate steps
-            rm 03-bowtie2/$j.sam
-            rm 03-bowtie2/$j.bam
+            lofreq call -f ${REFERENCE} -o 04-vcf/$j.vcf 03-bowtie2/$j.sorted.bam
     fi
 done
+
+# update the vcf
+# run snp on the updated vcf
+#
+#
