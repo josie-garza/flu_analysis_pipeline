94c83432f7aad29cd5b5cfb867ebfd7ade9c7625
Josie Garza <jpg7@rice.edu>
2019-04-23 18:31:44 -0500

    get samples

---
+++ UMDA/script_dev/04-vcf/extract.py
@@ -0,0 +1,20 @@
+
+# {seg: [(position, ALT)...]}
+
+def main():
+    summary = {}
+    f = open("00001.vcf", "r")
+    f1 = f.readlines()
+    for line in f1:
+        if line[0:2] == "NC":
+            info = line.split()
+            chromosome = info[0]
+            if chromosome in summary:
+                summary[chromosome].append((info[1], info[3]))
+            else:
+                summary[chromosome] = []
+                summary[chromosome].append((info[1], info[3]))
+    print(summary)
+
+if __name__== "__main__":
+    main()
